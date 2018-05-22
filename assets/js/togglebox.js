992e8fa783364c0e294b4774a8c64eec8479b55c
Arnaud LG <arnaud.lgrd@gmail.com>
2018-05-22 11:41:26 +0200

    Responsive width < 900px #2
    Modification de togglebox.js pour limiter la hauteur des boîtes déroulantes

--- assets/js/togglebox.js
+++ assets/js/togglebox.js
@@ -1,17 +1,23 @@
 $(function(){
+	/* Limitation de la hauteur des encarts 'commentaires' et 'forum' à sa valeur de base
+	* 	pour éviter le déroulement des 2 boîtes en cas d'alignement horizontal (responsive)
+	*/ 
+	var box_height = $('#commentsAndForum #forum').outerHeight();		
+	$('#commentsAndForum').children().css('max-height',box_height+'px');
+
 	$('#commentsAndForum .box_title').click(function(){
 		var show = $(this).parent().children('.box_content').attr('data_show');
 		if(show == 'no'){
 			$(this).parent().children('.box_content').attr('data_show','yes');
-			toggle('down',$(this));
+			toggle('down',$(this),box_height);
 		}else{
 			$(this).parent().children('.box_content').attr('data_show','no');
-			toggle('up',$(this));
+			toggle('up',$(this),box_height);
 		}
 	});
 });
 
-function toggle(sens,element)
+function toggle(sens,element,box_height)
 	{
 		var step = 0;
 		var y = 0;
@@ -20,17 +26,20 @@ function toggle(sens,element)
 		{
 			if(step>=135){
 				clearInterval(I);
+				if (sens == 'up') 
+				{
+					element.parent().css('max-height',box_height+'px');
+				}
 			}else{
 				step+=5;
 				y = step*50/135;
 				if(sens == 'down'){
 					element.find('.box_plus').css('transform','rotate(-'+step+'deg)');
 					element.parent().children('.box_content').css('max-height',y+'em');
-					console.log(element.parent().children('.box_content').css('max-height'));
+					element.parent().css('max-height','none');
 				}else{
 					element.find('.box_plus').css('transform','rotate(-'+(135-step)+'deg)');
 					element.parent().children('.box_content').css('max-height',50-y+'em');
-					console.log(element.parent().children('.box_content').css('max-height'));				
 				}
 				
 			}
