b844c610b41470dc6446acfee2a5dc222cd61a93
shangril <shangril@users.noreply.github.com>
2025-04-07 13:29:05 +0200

    Removed outdated social links

--- accueil/socials.php
+++ accueil/socials.php
@@ -1,26 +1,12 @@
 <?php
   // Socials
   $socials = [
-    'twitter' => [
-      'name' => [
-        'fr' => 'Twitter',
-        'en' => 'Twitter',
-      ],
-      'url' => 'https://twitter.com/dogmazic',
-    ],
     'mastodon' => [
       'name' => [
         'fr' => 'Mastodon',
         'en' => 'Mastodon',
       ],
       'url' => 'https://framapiaf.org/@dogmazic',
-    ],
-    'diaspora' => [
-      'name' => [
-        'fr' => 'Diaspora',
-        'en' => 'Diaspora',
-      ],
-      'url' => 'https://diasp.org/people/c3227c60bd1801328d8500259069449e',
     ],
     'blog' => [
       'name' => [
