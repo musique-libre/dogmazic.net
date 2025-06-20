2073bef8d2d32f3c422ea9bca6638e6fcae73c38
Alan <fufroma@gmail.com>
2025-06-20 07:53:38 +0200

    New art url from Ampache 7.5.2

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -479,7 +479,7 @@ function albumList(){
             //$image = str_replace('https://', '//', $item->getElementsByTagName('image')->item(0)->nodeValue); // http and https available
             // Ampache >= 7.4.0, no more <image> tag
             $album_id = str_replace('album-', '', $item->getElementsByTagName('guid')->item(0)->nodeValue);
-            $image = "//play.dogmazic.net/image.php?object_id=".$album_id."&object_type=album&thumb=2";
+            $image = "//play.dogmazic.net/image.php?object_id=" . $album_id . "&object_type=album&size=200x200";
 
             $link = $item->getElementsByTagName('link')->item(0)->nodeValue;
             $description = $item->getElementsByTagName('description')->item(0)->nodeValue;
